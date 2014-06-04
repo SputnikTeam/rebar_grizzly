dda30c1c758bcbef18cc7f62c06cfbc690a17716
kudryashov <kudryashov.sv@yandex.ru>
2014-06-04 15:08:13 +0400

    refactoring

--- src/grizzly.erl
+++ src/grizzly.erl
@@ -24,6 +24,7 @@ grizzly(Config, AppFile) ->
                     sync_modules(AppName, Nodes, Modules)
             end;
         false ->
+            io:format("not app - ~p~n", [AppFile]),
             ok
     end.
 
